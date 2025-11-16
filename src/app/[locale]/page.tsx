0fd2112f28b1ef82323123e1b54570f9ceed1f7b
Felix S <felix.e.schmitt.fes@gmail.com>
2025-11-16 15:45:36 +0100

    moved datasets page to dataset route, added main page route that routes to datasets

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,27 +1,6 @@
-import DatasetSearch from "../../components/dataset/DatasetSearch";
-import React, { Suspense } from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import SupportSection from "@/components/SupportSection";
+import DatasetsPage from "@/app/[locale]/dataset/page";
 
-export default async function Home() {
-  const urls = {
-    SEARCH: process.env.SEARCH_HUB_URL!,
-    REPO: process.env.REPO_HUB_URL!,
-  };
 
-  return (
-    <div className="bg-background w-full max-w-[1920px] mx-auto shadow-[0_0_12px_rgba(0,0,0,0.17)]">
-      <Header />
-      <div className="px-10 pt-20 w-full max-w-7xl mx-auto">
-        <Suspense>
-          <DatasetSearch urls={urls} />
-        </Suspense>
-      </div>
-
-      <SupportSection />
-
-      <Footer />
-    </div>
-  );
+export default function MainPage() {
+  return DatasetsPage()
 }
