60dfb5690338a6e5fc4ff60dcfafe2e2883b50cd
Felix S <felix.e.schmitt.fes@gmail.com>
2026-01-27 15:39:17 +0100

    improved dataset card

--- src/components/dataset/DatasetCard.tsx
+++ src/components/dataset/DatasetCard.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { useLocale } from "@/hooks/useLocale";
-import { Dataset, isAIModel, parseDate } from "@/lib/utils";
+import { Dataset, isAIModel } from "@/lib/utils";
 
 import {
   Card,
@@ -12,6 +12,9 @@ import {
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import HtmlSnippet from "@/components/HTMLSnippet";
+import CatalogBadge from "./CatalogBadge";
+import PublisherPopover from "./PublisherPopover";
+import DateBadge from "./DateBadge";
 
 interface Props {
   dataset: Dataset;
@@ -21,28 +24,34 @@ export default function DatasetCard({ dataset }: Props) {
   const { translateDict, translations, locale } = useLocale();
 
   const isModel = isAIModel(dataset);
-  const date = parseDate(dataset.modified) || parseDate(dataset.issued);
-  const age = date
-    ? Math.floor(
-        Math.abs(new Date().getTime() - date.getTime()) / (1000 * 60 * 60 * 24),
-      )
-    : null;
 
   return (
     <Link
       href={`/${locale}/${isModel ? "model" : "dataset"}/${dataset.id}`}
       className="w-full"
     >
-      <Card className="w-full hover:border-primary hover:bg-card/60 transition-all duration-200 cursor-pointer">
-        <CardHeader>
+      <Card className="w-full hover:border-primary hover:bg-card/60 transition-all duration-200 cursor-pointer gap-3">
+        <CardHeader className="gap-3">
           <CardTitle>
             <h2 className="text-2xl text-wrap">
               {translateDict(dataset.title)}
             </h2>
           </CardTitle>
+          <div className="flex gap-2 items-center justify-between">
+            <div onClick={(e) => e.preventDefault()}>
+              <PublisherPopover
+                publisher={dataset.publisher}
+                contact_point={dataset.contact_point}
+              />
+            </div>
+            <div className="flex gap-2 items-center">
+              <DateBadge modified={dataset.modified} issued={dataset.issued} />
+              <CatalogBadge catalog={dataset.catalog} />
+            </div>
+          </div>
         </CardHeader>
         <CardContent>
-          <div className="flex gap-2 justify-between">
+          <div className="flex flex-col gap-4 justify-between">
             <CardDescription className="flex-2/3 snippet">
               <HtmlSnippet
                 html={
@@ -58,13 +67,6 @@ export default function DatasetCard({ dataset }: Props) {
                   className="bg-amber-100 dark:bg-amber-500 text-amber-800 dark:text-black border-amber-300"
                 >
                   {translations.dataset.aiModel}
-                </Badge>
-              )}
-              <Badge variant={"outline"}>{date?.toLocaleDateString()}</Badge>
-              {(parseDate(dataset.modified) || parseDate(dataset.issued)) !=
-                null && (
-                <Badge variant={"outline"}>
-                  {age} {translations.dataset.daysOld}
                 </Badge>
               )}
               {[
