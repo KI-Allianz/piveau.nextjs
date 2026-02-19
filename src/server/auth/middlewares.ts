71eb2bb14955ebcd4a066f91c9bf8ea07c5d5342
Felix S <felix.e.schmitt.fes@gmail.com>
2026-02-19 17:13:19 +0100

    fixed package lock, fix disabled auth in trpc

--- src/server/auth/middlewares.ts
+++ src/server/auth/middlewares.ts
@@ -1,20 +1,21 @@
-import {TRPCError} from "@trpc/server";
+import { TRPCError } from "@trpc/server";
 
-import {AuthedTRPCContext} from "@/server/auth/types";
-import {t} from "@/server/trpc";
+import { AuthedTRPCContext } from "@/server/auth/types";
+import { t } from "@/server/trpc";
+const AUTH_DISABLED = process.env.AUTH_DISABLED === "true";
 
 // Middleware: require auth
 export const enforceUserIsAuthed = t.middleware(({ ctx, next }) => {
   const { session } = ctx;
-  if (!session || !session.user) {
+  if (!AUTH_DISABLED && (!session || !session.user)) {
     throw new TRPCError({ code: "UNAUTHORIZED", message: "Login required." });
   }
   return next({
     ctx: {
       ...ctx,
       // from here on, these are non-nullable in downstream resolvers
-      session,              // Session (not null)
-      user: session.user,   // add a convenient shortcut
+      session, // Session (not null)
+      user: session?.user, // add a convenient shortcut
     } as AuthedTRPCContext,
   });
 });
