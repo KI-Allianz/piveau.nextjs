9c499ff98a5d92cba86460fdccc0b258a1ab8520
Felix Schmitt <30461-felixelmar.schmitt@users.noreply.gitlab.gwdg.de>
2026-06-13 18:32:43 +0200

    updated api error handling

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,19 +10,24 @@ import { protectedProcedure, publicProcedure } from "./auth/procedures";
 
 export const appRouter = router({
   categories: publicProcedure.query(async () => {
-    const res = await searchResource<SearchResult<Dataset>>({
-      baseUrl: process.env.SEARCH_HUB_URL!,
-      params: {
-        q: "",
-        filters: "dataset",
-        limit: 1,
-        page: 0,
-        includes: ["categories.label"],
-      },
-    });
+    try {
+      const res = await searchResource<SearchResult<Dataset>>({
+        baseUrl: process.env.SEARCH_HUB_URL!,
+        params: {
+          q: "",
+          filters: "dataset",
+          limit: 1,
+          page: 0,
+          includes: ["categories.label"],
+        },
+      });
 
-    return res.data.result.facets.find((facet) => facet.id === "categories")
-      ?.items;
+      return res.data.result.facets.find((facet) => facet.id === "categories")
+        ?.items;
+    } catch (error) {
+      console.error("Failed to fetch categories:", error);
+      throw new Error("Failed to fetch categories from Search Hub Upstream");
+    }
   }),
   search: {
     datasets: publicProcedure.input(SearchParamsSchema).query(async (opts) => {
@@ -39,47 +44,52 @@ export const appRouter = router({
         };
       }
 
-      const res = await searchResource<SearchResult<Dataset>>({
-        baseUrl: process.env.SEARCH_HUB_URL || "",
-        params: {
-          ...input,
-          filters: "dataset",
-          includes: [
-            "id",
-            "title",
-            "description",
-            "languages",
-            "modified",
-            "issued",
-            "catalog.id",
-            "catalog.title",
-            "catalog.country.id",
-            "distributions.id",
-            "distributions.format.label",
-            "distributions.format.id",
-            "distributions.license",
-            "categories.label",
-            "keywords.label",
-            "publisher",
-          ],
-        },
-        // axiosInstance,
-      });
-
-      // Filter results again | temporary solution
-      if (!isAuthed) {
-        res.data.result.results = res.data.result.results.filter((item) => {
-          const keywords = item?.keywords || [];
-          return keywords.some((keyword) => keyword.label === "public");
+      try {
+        const res = await searchResource<SearchResult<Dataset>>({
+          baseUrl: process.env.SEARCH_HUB_URL || "",
+          params: {
+            ...input,
+            filters: "dataset",
+            includes: [
+              "id",
+              "title",
+              "description",
+              "languages",
+              "modified",
+              "issued",
+              "catalog.id",
+              "catalog.title",
+              "catalog.country.id",
+              "distributions.id",
+              "distributions.format.label",
+              "distributions.format.id",
+              "distributions.license",
+              "categories.label",
+              "keywords.label",
+              "publisher",
+            ],
+          },
+          // axiosInstance,
         });
 
-        // No data leak of facets
-        if (res.data.result.results.length === 0) {
-          res.data.result.facets = [];
-        }
-      }
+        // Filter results again | temporary solution
+        if (!isAuthed) {
+          res.data.result.results = res.data.result.results.filter((item) => {
+            const keywords = item?.keywords || [];
+            return keywords.some((keyword) => keyword.label === "public");
+          });
 
-      return res.data.result;
+          // No data leak of facets
+          if (res.data.result.results.length === 0) {
+            res.data.result.facets = [];
+          }
+        }
+
+        return res.data.result;
+      } catch (error) {
+        console.error("Search Resource Failed:", error);
+        throw new Error("Failed to fetch from Search Hub Upstream");
+      }
     }),
     catalogs: publicProcedure.input(SearchParamsSchema).query(async (opts) => {
       const { input, ctx } = opts;
@@ -95,25 +105,30 @@ export const appRouter = router({
       //   };
       // }
 
-      const res = await searchResource<SearchResult<Catalog>>({
-        baseUrl: process.env.SEARCH_HUB_URL || "",
-        params: {
-          ...input,
-          filters: "catalogue",
-          includes: [
-            "id",
-            "title",
-            "description",
-            "modified",
-            "issued",
-            "country",
-            "count",
-            "keywords.label",
-          ],
-        },
-      });
+      try {
+        const res = await searchResource<SearchResult<Catalog>>({
+          baseUrl: process.env.SEARCH_HUB_URL || "",
+          params: {
+            ...input,
+            filters: "catalogue",
+            includes: [
+              "id",
+              "title",
+              "description",
+              "modified",
+              "issued",
+              "country",
+              "count",
+              "keywords.label",
+            ],
+          },
+        });
 
-      return res.data.result;
+        return res.data.result;
+      } catch (error) {
+        console.error("Search Resource Failed:", error);
+        throw new Error("Failed to fetch from Search Hub Upstream");
+      }
     }),
   },
 });
