71b43aa231868504e5184df050b0b10d0a056f8a
Felix Schmitt <30461-felixelmar.schmitt@users.noreply.gitlab.gwdg.de>
2026-08-10 12:58:42 +0200

    add search icon to search bar

--- src/components/homepage/SearchPreview.tsx
+++ src/components/homepage/SearchPreview.tsx
@@ -3,16 +3,21 @@
 import { useLocale } from "@/hooks/useLocale";
 import { Input } from "../ui/input";
 import { useState } from "react";
+import { Search } from "lucide-react";
 
 export const SearchPreview = () => {
   const { translations, locale } = useLocale();
   const [query, setQuery] = useState("");
 
   return (
-    <form action={`/${locale}/dataset/`} className="w-full">
+    <form action={`/${locale}/dataset/`} className="w-full relative">
+      <Search
+        className="absolute left-8 top-1/2 -translate-y-1/2 text-muted-foreground"
+        size={22}
+      />
       <Input
         placeholder={translations.search.placeholder.datasets}
-        className="w-full placeholder:text-xl bg-card p-8 rounded-lg shadow text-xl!"
+        className="w-full placeholder:text-xl bg-card p-8 rounded-lg shadow text-xl! pl-15"
         role={"search"}
         name={"q"}
         value={query}
