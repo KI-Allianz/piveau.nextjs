7557dfc64a19047e1c4c830219714ad92f4d7083
Felix S <felix.e.schmitt.fes@gmail.com>
2026-08-13 12:09:58 +0200

    added dummy text under title

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -26,6 +26,17 @@ export default async function MainPage({ params }: Props) {
         <h1 className="text-4xl font-bold mb-6 text-center">
           {theme.lang.translations[locale]?.title || ""}
         </h1>
+        <div className="w-full flex justify-center pb-4">
+          <span className="max-w-2/3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </span>
+        </div>
 
         <div className="flex flex-col gap-4">
           <SearchPreview />
